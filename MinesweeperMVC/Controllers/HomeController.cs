cadacad5bb5cfcd0f0dc8dd058a0e7de1ab1fd50
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-05-06 18:12:50 -0400

    Add project files.

---
+++ MinesweeperMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinesweeperMVC.Models;
+
+namespace MinesweeperMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static MinesweeperModel _mm;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Game()
+        {
+            _mm = new MinesweeperModel(10, 5, 8);
+            return View(model: _mm);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Move(int row, int col)
+        {
+            _mm.Board[row, col]++;
+            return View("Game",_mm);
+        }
+    }
+}
---
+++ MinesweeperMVC/Models/MinesweeperModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinesweeperMVC.Models
+{
+    public class MinesweeperViewModel
+    {
+    }
+
+    public class MinesweeperModel
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public int CellCount => Rows * Columns;
+        public int BombCount { get; set; }
+
+        public int MoveCount
+        {
+            get
+            {
+                return Board.Cast<int>().Count(c => c != 0);
+            }
+        }
+
+        public MinesweeperModel(int rows, int columns, int bombCount)
+        {
+            Rows = rows;
+            Columns = columns;
+            BombCount = bombCount;
+            Board = new int[Rows, Columns];
+        }
+
+        public int[,] Board;
+
+        public string Display(in int row, in int col)
+        {
+            switch (Board[row, col])
+            {
+                case 0: return "_";
+                case 1: return "\u2222";
+                case 2: return "\u2652";
+            }
+            return "A";
+        }
+    }
+}
